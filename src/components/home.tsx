62e2af7c9ea17519b40ca3bf555dc8647a4fbce6
Jazz <alllegron@gmail.com>
2024-04-08 20:39:38 +0000

    Added a navbar to the page

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -3,33 +3,45 @@ import { useState } from 'react';
 import reactLogo from '../assets/react.svg';
 import viteLogo from '/vite.svg';
 import MainHeader from './main_header';
+import ResponsiveAppBar from './navbar';
+import Box from '@mui/material/Box';
 
 const Home: React.FC = () => {
     const [count, setCount] = useState(0);
 
     return (
         <>
-            <MainHeader title="John Cinquegrana" />
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
+            {/* A full width div to lay out everything in columns underneath it */}
+            <Box sx={{
+                flexFlow: 'column nowrap',
+                justifyContent: 'flex-start',
+                alignItems: 'stretch',
+                width: '100%',
+            }} >
+                <ResponsiveAppBar />
+                <MainHeader title="John Cinquegrana" />
+
+                <div>
+                    <a href="https://vitejs.dev" target="_blank">
+                        <img src={viteLogo} className="logo" alt="Vite logo" />
+                    </a>
+                    <a href="https://react.dev" target="_blank">
+                        <img src={reactLogo} className="logo react" alt="React logo" />
+                    </a>
+                </div>
+                <h1>Vite + React</h1>
+                <div className="card">
+                    <button onClick={() => setCount((count) => count + 1)}>
+                        count is {count}
+                    </button>
+                    <p>
+                        Edit <code>src/App.tsx</code> and save to test HMR
+                    </p>
+                </div>
+                <p className="read-the-docs">
+                    Click on the Vite and React logos to learn more
                 </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+            </Box>
         </>
     );
 };
