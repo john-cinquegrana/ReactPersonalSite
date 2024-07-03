5bdec47914ad9247b87dfa2397bbc19c7f0c02fa
John Cinquegrana <john.cinquegrana@rutgers.edu>
2024-07-03 19:21:39 +0000

    New Fonts added

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,9 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+// Supports weights 400-700
+import '@fontsource-variable/lora';
+import '@fontsource/ubuntu';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
