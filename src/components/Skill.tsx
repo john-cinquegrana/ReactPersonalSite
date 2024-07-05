71bdb68f6c2d343ec9c68340ec0a0098388a0206
John Cinquegrana <john.cinquegrana@rutgers.edu>
2024-07-05 23:28:32 +0000

    Formatted skills file

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,40 +1,36 @@
-
 import { Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import { Box } from '@mui/system';
 
-
 interface SkillProps {
-    color?: string;
-    label: string;
+	color?: string;
+	label: string;
 }
 
 const Skill: React.FC<SkillProps> = ({ color, label }) => {
-    const theme = useTheme();
+	const theme = useTheme();
 
-    // Use a default color if one is not passed in
-    let usedColor: string;
+	// Use a default color if one is not passed in
+	let usedColor: string;
 
-    if (!color) {
-        usedColor = theme.palette.secondaryContainer;
-    }
-    else {
-        usedColor = color;
-    }
+	if (!color) {
+		usedColor = theme.palette.secondaryContainer;
+	} else {
+		usedColor = color;
+	}
 
-    return (
-        <Box sx={{
-            backgroundColor: usedColor,
-            color: theme.palette.onSecondaryContainer,
-            padding: '6px',
-            borderRadius: '6px',
-        }}>
-            <Typography
-                variant='body1'
-
-            >{label}</Typography>
-        </Box>
-    );
+	return (
+		<Box
+			sx={{
+				backgroundColor: usedColor,
+				color: theme.palette.onSecondaryContainer,
+				padding: '6px',
+				borderRadius: '6px',
+			}}
+		>
+			<Typography variant='body1'>{label}</Typography>
+		</Box>
+	);
 };
 
 export default Skill;
