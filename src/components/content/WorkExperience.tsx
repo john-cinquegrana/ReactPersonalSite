9caa8eb02855686c5652bc7e863f62f919efd46a
John Cinquegrana <john.cinquegrana@rutgers.edu>
2024-07-05 23:01:30 +0000

    Wrapped list elements with Typography

--- src/components/content/WorkExperience.tsx
+++ src/components/content/WorkExperience.tsx
@@ -25,10 +25,10 @@ const WorkExperience: React.FC<WorkExperienceProps> = () => {
                 <Typography variant='body1'>An all-encompassing 911 and emergency management solution</Typography>
                 <Typography variant='h6'>Experiences</Typography>
                 <ul>
-                    <li>Lead architecture, design, and development, of a Flutter-based mobile application deployed to Android and iOS</li>
-                    <li>Integrated into Aruba IoT to dynamically track phones across WLAN for the purposes of locating 911 calls</li>
-                    <li>Lead developers in high stress scenarios to provide necessary hotfixes on day-of-discovery for website vulnerabilities</li>
-                    <li>Designed CI/CD for deployment of mobile apps to both testers, and app stores, using Firebase and AWS CodeStar</li>
+                    <li><Typography variant='body1'>Lead architecture, design, and development, of a Flutter-based mobile application deployed to Android and iOS</Typography></li>
+                    <li><Typography variant='body1'>Integrated into Aruba IoT to dynamically track phones across WLAN for the purposes of locating 911 calls</Typography></li>
+                    <li><Typography variant='body1'>Lead developers in high stress scenarios to provide necessary hotfixes on day-of-discovery for website vulnerabilities</Typography></li>
+                    <li><Typography variant='body1'>Designed CI/CD for deployment of mobile apps to both testers, and app stores, using Firebase and AWS CodeStar</Typography></li>
                 </ul>
                 <Typography variant='h6'>Relevant Skills</Typography>
                 <Box>
