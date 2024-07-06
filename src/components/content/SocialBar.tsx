e86610fef86731fb600130d3eba04ba81ead6252
John Cinquegrana <john.cinquegrana@rutgers.edu>
2024-07-06 19:35:09 +0000

    Social Bar has some cool styling

--- src/components/content/SocialBar.tsx
+++ src/components/content/SocialBar.tsx
@@ -1,12 +1,12 @@
 import { LinkedIn } from '@mui/icons-material';
 import GitHub from '@mui/icons-material/GitHub';
-import { Typography, Box } from '@mui/material';
+import { Typography, Box, SxProps, Theme } from '@mui/material';
 
 // An interface that contains the social text, icon, and link
 interface SocialProps {
 	text: string;
 	icon: React.ReactNode;
-	link: string;
+	link?: string;
 }
 
 const SocialBar = () => {
@@ -14,9 +14,13 @@ const SocialBar = () => {
 		{
 			text: 'Github',
 			icon: <GitHub />,
-			link: 'github.com',
+			link: 'https://github.com/john-cinquegrana',
 		},
-		{ text: 'LinkedIn', icon: <LinkedIn />, link: 'linkedin.com' },
+		{
+			text: 'LinkedIn',
+			icon: <LinkedIn />,
+			link: 'https://www.linkedin.com/in/jcinquegrana/',
+		},
 	];
 
 	return (
@@ -25,7 +29,13 @@ const SocialBar = () => {
 				color: 'white',
 				backgroundColor: 'black',
 				width: '100%',
-				height: '2em',
+				padding: {
+					xs: '4px 0px',
+					sm: '6px 0px',
+					md: '8px 0px',
+					lg: '9px 0px',
+					xl: '10px 0px',
+				},
 				display: 'flex',
 				flexFlow: 'row nowrap',
 				justifyContent: 'space-around',
@@ -34,21 +44,43 @@ const SocialBar = () => {
 
 			// ...
 		>
-			{socials.map((social, index) => (
-				// A Box containing the relevant icon and the text
-				<Box
-					key={index}
-					sx={{
-						display: 'flex',
-						flexFlow: 'row nowrap',
-						justifyContent: 'center',
-						alignItems: 'center',
-					}}
-				>
-					{social.icon}
-					<Typography variant='subtitle1'>{social.text}</Typography>
-				</Box>
-			))}
+			{socials.map((social, index) => {
+				// The styilng that will be used for the container of the icon and text
+				const boxStyling: SxProps<Theme> = {
+					display: 'flex',
+					flexFlow: 'row nowrap',
+					justifyContent: 'center',
+					alignItems: 'center',
+				};
+				// The function to be called when this element is clicked on
+				let onClick = undefined;
+				// Add extra values if we actually have a link to follow
+				if (social.link != undefined) {
+					boxStyling.cursor = 'pointer';
+					onClick = () => window.open(social.link);
+				}
+				return (
+					// A Box containing the relevant icon and the text
+					<Box
+						key={index}
+						sx={boxStyling}
+						// Only do something with the link if we have a function to call
+						onClick={onClick}
+					>
+						{social.icon}
+						<Typography
+							variant='subtitle2'
+							sx={{
+								fontStyle: 'normal',
+								opacity: 1,
+								paddingLeft: '6px',
+							}}
+						>
+							{social.text}
+						</Typography>
+					</Box>
+				);
+			})}
 		</Box>
 	);
 };
