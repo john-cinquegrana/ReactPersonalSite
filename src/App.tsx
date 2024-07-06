7c4efbc1ef72c1398ef33e92724d56d5d159718a
John Cinquegrana <john.cinquegrana@rutgers.edu>
2024-07-06 20:17:43 +0000

    Added CSS baseline to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import './App.css';
 import Home from './components/pages/Home';
 import { lightTheme } from './components/theme';
@@ -6,6 +6,7 @@ import { lightTheme } from './components/theme';
 function App() {
 	return (
 		<ThemeProvider theme={lightTheme}>
+			<CssBaseline />
 			<Home />
 		</ThemeProvider>
 	);
