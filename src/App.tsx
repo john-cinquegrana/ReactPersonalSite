81cfab8197d098656ddfeb42869384962ebbdf70
John Cinquegrana <john.cinquegrana@rutgers.edu>
2024-08-20 19:22:30 +0000

    Installed and made use of React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
 import '@root/App.css';
 import Home from '@pages/Home';
 import { lightTheme } from '@components/theme';
 
 function App() {
 	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path='/'
+					element={<Layout />}
+				>
+					<Route
+						index
+						element={<Home />}
+					/>
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+function Layout() {
+	return (
 		<ThemeProvider theme={lightTheme}>
 			<CssBaseline />
-			<Home />
+			<Outlet />
 		</ThemeProvider>
 	);
 }
