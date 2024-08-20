12d7e546196507de7f8318d1c0398c1e6614d27c
John Cinquegrana <john.cinquegrana@rutgers.edu>
2024-08-20 19:54:41 +0000

    Restyled the social bar

--- src/components/content/SocialBar.tsx
+++ src/components/content/SocialBar.tsx
@@ -1,5 +1,12 @@
 import { Email, GitHub, LinkedIn, Place } from '@mui/icons-material';
-import { Typography, Box, SxProps, Theme } from '@mui/material';
+import {
+	Typography,
+	Box,
+	SxProps,
+	Theme,
+	Divider,
+	useTheme,
+} from '@mui/material';
 
 // An interface that contains the social text, icon, and link
 interface SocialProps {
@@ -31,64 +38,98 @@ const SocialBar = () => {
 		},
 	];
 
+	const theme = useTheme();
+
+	const dividerMargin = '4px';
+	const dividerColor = theme.palette.secondaryContainer;
+
 	return (
 		<Box
 			sx={{
-				color: 'white',
-				backgroundColor: 'black',
-				width: '100%',
-				padding: {
-					xs: '4px 0px',
-					sm: '6px 0px',
-					md: '8px 0px',
-					lg: '9px 0px',
-					xl: '10px 0px',
-				},
 				display: 'flex',
-				flexFlow: 'row nowrap',
-				justifyContent: 'space-evenly',
+				flexFlow: 'column nowrap',
+				justifyContent: 'space-between',
 				alignItems: 'center',
+				margin: 0,
+				padding: 0,
 			}}
-
-			// ...
 		>
-			{socials.map((social, index) => {
-				// The styilng that will be used for the container of the icon and text
-				const boxStyling: SxProps<Theme> = {
+			<Divider
+				sx={{
+					borderBottomWidth: '2px',
+					borderColor: dividerColor,
+					marginBottom: dividerMargin,
+					width: '88%',
+				}}
+				variant='fullWidth'
+			></Divider>
+			<Box
+				sx={{
+					color: theme.palette.secondary.dark,
+					backgroundColor: 'transparent',
+					width: '100%',
+					padding: {
+						xs: '4px 0px',
+						sm: '6px 0px',
+						md: '8px 0px',
+						lg: '9px 0px',
+						xl: '10px 0px',
+					},
 					display: 'flex',
 					flexFlow: 'row nowrap',
-					justifyContent: 'center',
+					justifyContent: 'space-evenly',
 					alignItems: 'center',
-				};
-				// The function to be called when this element is clicked on
-				let onClick = undefined;
-				// Add extra values if we actually have a link to follow
-				if (social.link != undefined) {
-					boxStyling.cursor = 'pointer';
-					onClick = () => window.open(social.link);
-				}
-				return (
-					// A Box containing the relevant icon and the text
-					<Box
-						key={index}
-						sx={boxStyling}
-						// Only do something with the link if we have a function to call
-						onClick={onClick}
-					>
-						{social.icon}
-						<Typography
-							variant='subtitle2'
-							sx={{
-								fontStyle: 'normal',
-								opacity: 1,
-								paddingLeft: '6px',
-							}}
+				}}
+
+				// ...
+			>
+				{socials.map((social, index) => {
+					// The styilng that will be used for the container of the icon and text
+					const boxStyling: SxProps<Theme> = {
+						display: 'flex',
+						flexFlow: 'row nowrap',
+						justifyContent: 'center',
+						alignItems: 'center',
+					};
+					// The function to be called when this element is clicked on
+					let onClick = undefined;
+					// Add extra values if we actually have a link to follow
+					if (social.link != undefined) {
+						boxStyling.cursor = 'pointer';
+						onClick = () => window.open(social.link);
+					}
+					return (
+						// A Box containing the relevant icon and the text
+						<Box
+							key={index}
+							sx={boxStyling}
+							// Only do something with the link if we have a function to call
+							onClick={onClick}
 						>
-							{social.text}
-						</Typography>
-					</Box>
-				);
-			})}
+							{social.icon}
+							<Typography
+								variant='subtitle2'
+								sx={{
+									fontStyle: 'normal',
+									opacity: 1,
+									paddingLeft: '6px',
+								}}
+							>
+								{social.text}
+							</Typography>
+						</Box>
+					);
+				})}
+			</Box>
+			<Divider
+				sx={{
+					marginTop: dividerMargin,
+					borderColor: dividerColor,
+					borderBottomWidth: '3px',
+					width: '90%',
+				}}
+				variant='fullWidth'
+			></Divider>
 		</Box>
 	);
 };
