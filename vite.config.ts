dfb26a1d46b4379c92d0d8fc35463eaaf449b109
John Cinquegrana <john.cinquegrana@rutgers.edu>
2024-08-20 19:14:21 +0000

    Added the pages route

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
 		alias: {
 			'@root': path.resolve(__dirname, './src'),
 			'@components': path.resolve(__dirname, './src/components'),
+			'@pages': path.resolve(__dirname, './src/pages'),
 		},
 	},
 });
