025db9a9207240f9f43dab075f9a8011c14db827
Jazz <alllegron@gmail.com>
2024-09-15 22:04:47 +0000

    ignored bad errors in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// @ts-expect-error path is 100% installed I don't know why it complains
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
@@ -12,8 +13,11 @@ export default defineConfig({
 	},
 	resolve: {
 		alias: {
+			// @ts-expect-error __dirname isn't caught by typescript
 			'@root': path.resolve(__dirname, './src'),
+			// @ts-expect-error __dirname isn't caught by typescript
 			'@components': path.resolve(__dirname, './src/components'),
+			// @ts-expect-error __dirname isn't caught by typescript
 			'@pages': path.resolve(__dirname, './src/pages'),
 		},
 	},
