25cddf0d2a222561ae4d7df729df9dab64706e4e
Jazz <alllegron@gmail.com>
2024-09-15 21:50:02 +0000

    Circle can now be changed by sx

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,18 +1,23 @@
+import { Box } from '@mui/material';
 import React from 'react';
+import { SxProps } from '@mui/system';
 
 interface CircleProps {
     color: string;
     radius: number;
+    sx?: SxProps;
 }
 
 /**
  * @returns A circle element as an SVG.
  */
-const Circle: React.FC<CircleProps> = ({ color, radius }) => {
+const Circle: React.FC<CircleProps> = ({ color, radius, sx }) => {
     return (
-        <svg width={radius * 2} height={radius * 2}>
-            <circle cx={radius} cy={radius} r={radius} fill={color} />
-        </svg>
+        <Box sx={sx}>
+            <svg width={radius * 2} height={radius * 2}>
+                <circle cx={radius} cy={radius} r={radius} fill={color} />
+            </svg>
+        </Box>
     );
 };
 
