08653b5e363ccbadfabd22cd20cb4d316f13966e
Jazz <alllegron@gmail.com>
2024-09-15 22:01:49 +0000

    Using polling to detect file changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	server: {
+		watch: {
+			usePolling: true,
+		}
+	},
 	resolve: {
 		alias: {
 			'@root': path.resolve(__dirname, './src'),
