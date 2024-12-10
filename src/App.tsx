d70636773aa703481f8ee9388af1a81f605e667f
John Cinquegrana <john.cinquegrana@rutgers.edu>
2024-12-10 21:40:45 +0000

    high level nextUI installation

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { CssBaseline, ThemeProvider } from '@mui/material';
 import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
 import '@root/App.css';
 import Home from '@pages/Home';
-import { lightTheme } from '@components/theme';
+import { NextUIProvider } from '@nextui-org/react';
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route
-					path='/'
-					element={<Layout />}
-				>
+		<NextUIProvider>
+			<BrowserRouter>
+				<Routes>
 					<Route
-						index
-						element={<Home />}
-					/>
-				</Route>
-			</Routes>
-		</BrowserRouter>
+						path='/'
+						element={<Layout />}
+					>
+						<Route
+							index
+							element={<Home />}
+						/>
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</NextUIProvider>
 	);
 }
 
 function Layout() {
-	return (
-		<ThemeProvider theme={lightTheme}>
-			<CssBaseline />
-			<Outlet />
-		</ThemeProvider>
-	);
+	return <Outlet />;
 }
 
 export default App;
