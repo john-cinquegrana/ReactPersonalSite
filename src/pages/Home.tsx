b1ce4d30fdb977e354c6dd7ce0d3582f804de76c
John Cinquegrana <john.cinquegrana@rutgers.edu>
2024-12-10 23:15:07 +0000

    Basic page with my name on it

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,16 @@
+import { Card, CardBody } from '@nextui-org/react';
 import React from 'react';
 
 const Home: React.FC = () => {
 	return (
-		<div className='flex-col bg-transparent'>
-			<h1 className='text-7xl bg-transparent'>John Cinquegrana</h1>
+		<div className='px-8'>
+			<Card className='bg-primary-100 text-center mx-8 rounded-t-none h-64'>
+				<CardBody className='flex flex-col justify-end align-middle'>
+					<h1 className='text-7xl bg-transparent p-8 text-primary-900 text-center'>
+						John Cinquegrana
+					</h1>
+				</CardBody>
+			</Card>
 		</div>
 	);
 };
