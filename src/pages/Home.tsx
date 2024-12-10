e4b4cc9ace69174f78e1e0161edf8493183e2c2f
John Cinquegrana <john.cinquegrana@rutgers.edu>
2024-12-10 22:43:00 +0000

    Tailwind and NextUI project start

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+	return (
+		<div className='flex-col bg-transparent'>
+			<h1 className='text-7xl bg-transparent'>John Cinquegrana</h1>
+		</div>
+	);
+};
+
+export default Home;
