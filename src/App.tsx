42c9c81fba4b9561aab63f4be110dd7c77a5a1c6
John Cinquegrana <john.cinquegrana@rutgers.edu>
2024-12-10 21:50:39 +0000

    Applied NextUI themeing at the top level

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
-import '@root/App.css';
+import '@root/output.css';
 import Home from '@pages/Home';
 import { NextUIProvider } from '@nextui-org/react';
 
 function App() {
 	return (
 		<NextUIProvider>
-			<BrowserRouter>
-				<Routes>
-					<Route
-						path='/'
-						element={<Layout />}
-					>
+			<main className='dark text-foreground bg-background'>
+				<BrowserRouter>
+					<Routes>
 						<Route
-							index
-							element={<Home />}
-						/>
-					</Route>
-				</Routes>
-			</BrowserRouter>
+							path='/'
+							element={<Layout />}
+						>
+							<Route
+								index
+								element={<Home />}
+							/>
+						</Route>
+					</Routes>
+				</BrowserRouter>
+			</main>
 		</NextUIProvider>
 	);
 }
