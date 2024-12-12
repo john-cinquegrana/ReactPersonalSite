4f51d232b931a6d880e780650d8bb4523859669d
Jazz <alllegron@gmail.com>
2024-12-12 23:35:08 +0000

    HMR now works in the dev container

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,23 @@ import React from 'react';
 
 const Home: React.FC = () => {
 	return (
-		<div className='px-8'>
-			<Card className='bg-primary-100 text-center mx-8 rounded-t-none h-64'>
-				<CardBody className='flex flex-col justify-end align-middle'>
+		<div className='px-8 flex flex-col justify-start content-stretch'>
+			<Card className='bg-primary-100 text-center rounded-t-none h-64'>
+				<CardBody className='flex flex-col justify-end content-center'>
 					<h1 className='text-7xl bg-transparent p-8 text-primary-900 text-center'>
 						John Cinquegrana
 					</h1>
+				</CardBody>
+			</Card>
+			<Card className='my-2'>
+				<CardBody>
+					<h2 className='text-4xl text-primary-900 text-center'>
+						Welcome to my personal website!
+					</h2>
+					<p className='text-lg text-primary-900 text-center'>
+						This is a work in progress, but I hope to have some
+						content up soon! HMR is a fucking bitch. Hello herro
+					</p>
 				</CardBody>
 			</Card>
 		</div>
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    watch: {
+      usePolling: true,
+      // useFsEvents: true,
+      // interval: 100,
+    },
+  },
   plugins: [react()],
 })
