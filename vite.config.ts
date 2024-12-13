9b9772afe0d1962c0a2c3aaa875e58128f51057a
Jazz <alllegron@gmail.com>
2024-12-12 19:02:45 -0500

    HMR is now actually working

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    watch: {
-      usePolling: true,
-      // useFsEvents: true,
-      // interval: 100,
-    },
-  },
-  plugins: [react()],
-})
+	plugins: [react()],
+});
