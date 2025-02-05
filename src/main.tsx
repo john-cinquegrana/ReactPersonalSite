082d8180ee6c5e58821c5964ea9c45c664e9f71f
Jazz <alllegron@gmail.com>
2025-02-05 13:45:51 -0500

    heroui migration

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { NextUIProvider } from '@nextui-org/react';
+import { HeroUIProvider } from "@heroui/react";
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<NextUIProvider>
+		<HeroUIProvider>
 			<main className='dark text-foreground bg-background w-full'>
 				<App />
 			</main>
-		</NextUIProvider>
+		</HeroUIProvider>
 	</StrictMode>,
 );
