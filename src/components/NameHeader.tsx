bf323b5bb54b24b4bbf4fd071228bd768daa52d2
John Cinquegrana <alllegron@gmail.com>
2025-02-07 14:10:24 -0500

    darkened header background

--- src/components/NameHeader.tsx
+++ src/components/NameHeader.tsx
@@ -5,7 +5,7 @@ import { BodyMedium, DisplayMedium } from './Typography';
 
 function NameHeader() {
 	return (
-		<GlassCard className='bg-primary-200 text-default-900 text-center my-16 py-8'>
+		<GlassCard className='bg-primary-100 text-default-900 text-center my-16 py-8'>
 			<CardBody className='flex flex-row justify-evenly content-center'>
 				<img
 					src={saxophone}
