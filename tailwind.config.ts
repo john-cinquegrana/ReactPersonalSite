14af3eae1a51cdaffc279850a6ed6ef6d711a509
John Cinquegrana <alllegron@gmail.com>
2025-02-06 23:46:52 -0500

    custom breakpoints in tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,33 @@
+import { heroui } from '@heroui/react';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		'./index.html',
+		'./src/**/*.{js,ts,jsx,tsx}',
+		'./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		screens: {
+			ss: '0',
+			// => @media (min-width: 0px) { ... }
+
+			xs: '480px',
+			// => @media (min-width: 480px) { ... }
+
+			sm: '640px',
+			// => @media (min-width: 640px) { ... }
+
+			md: '768px',
+			// => @media (min-width: 768px) { ... }
+
+			lg: '1024px',
+			// => @media (min-width: 1024px) { ... }
+
+			xl: '1280px',
+			// => @media (min-width: 1280px) { ... }
+		},
+	},
+	darkMode: 'class',
+	plugins: [heroui()],
+};
