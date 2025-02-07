94687f9e302a4758917b1d88a557dd0f3d186b2a
John Cinquegrana <alllegron@gmail.com>
2025-02-07 00:19:27 -0500

    Added an additional breakpoint

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import tailwindConfig from '../../tailwind.config';
 
 interface ContainerProps {
 	children: React.ReactNode;
 }
 
 const Container: React.FC<ContainerProps> = ({ children }) => {
-	// Generate the breakpoints that we need
-	let classNames = '';
-	// The super small breakpoint is custom since it's too small
-	classNames += 'ss:w-[400px] ';
-	// Pull all of the breakpoint values out of the
-	const breakpoints = tailwindConfig.theme.screens;
-	// Get a breakpoint class for each breakpoint declaration except ss.
-	const breakClasses = Object.entries(breakpoints)
-		.filter(([key]) => key !== 'ss')
-		.map(([key, value]) => `${key}:w-[${value}px]`)
-		.join(' ');
-	// Add the newly created breakpoint classes into the total list
-	classNames += breakClasses;
+	/* The weird automatic stuff I tried wasn't working because Tailwind
+	was optimizing out the breakpoint classes if no other component used them. */
 
-	return <div className={classNames}>{children}</div>;
+	// Generate the breakpoints that we need
+	// let classNames = '';
+	// // The super small breakpoint is custom since it's too small
+	// // Pull all of the breakpoint values out of the
+	// const breakpoints = tailwindConfig.theme.screens;
+	// // Get a breakpoint class for each breakpoint declaration except ss.
+	// const breakClasses = Object.entries(breakpoints)
+	// 	.filter(([key]) => key !== 'ss')
+	// 	.map(([key, value]) => `${key}:w-[${value}]`)
+	// 	.join(' ');
+	// // Add the newly created breakpoint classes into the total list
+	// classNames += breakClasses;
+	// classNames += ' ss:w-[400px]';
+
+	// console.log(classNames);
+
+	// const writtenNames = 'ss:w-[400px] xs:w-[480px] md:w-[768px]';
+
+	return (
+		<div className='ss:w-[400px] xs:w-[480px] md:w-[768px] lg:w-[1024px] xl:w-[1280px] 2xl:w-[1536px] 3xl:w-[1920px]'>
+			{children}
+		</div>
+	);
 };
 
 export default Container;
