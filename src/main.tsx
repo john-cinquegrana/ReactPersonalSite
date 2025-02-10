7b1e4de9363f38ec57f169ca5a0f7102e860031a
Jazz <alllegron@gmail.com>
2025-02-10 18:50:34 -0500

    Added Vercel's Analytics system

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { HeroUIProvider } from "@heroui/react";
+import { HeroUIProvider } from '@heroui/react';
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<HeroUIProvider>
 			<main className='dark text-foreground bg-background w-full'>
 				<App />
+				<Analytics />
 			</main>
 		</HeroUIProvider>
 	</StrictMode>,
