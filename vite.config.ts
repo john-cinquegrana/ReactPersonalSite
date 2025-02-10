f82599576d7ac4c890c699db0a7ee655fb476053
Jazz <alllegron@gmail.com>
2025-02-10 13:57:57 -0500

    Installed SVG packlage for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
@@ -6,5 +7,10 @@ export default defineConfig({
 	server: {
 		port: 5174,
 	},
-	plugins: [react()],
+	plugins: [
+		react(),
+		svgr({
+			include: '**/*.svg?react',
+		}),
+	],
 });
