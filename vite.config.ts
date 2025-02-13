f33394cdc9788952a1d25d63c58fdf693f46b357
Jazz <alllegron@gmail.com>
2025-02-12 20:50:46 -0500

    Tried and failed to get Tailwind v4 working. Will re-visit when official support for HeroUI is added

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+// import { heroui } from '@heroui/react';
 
 // https://vite.dev/config/
 export default defineConfig({
